403739d8abef2882d6c1e3be96f5b43f91370189
alehuo <aleksi.huotala@helsinki.fi>
2018-03-26 10:17:20 +0300

    Created basic project layout. Using React Bootstrap as the UI library. Fontawesome for icons.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MainPage } from "./pages/MainPage";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>Clubhouse management</Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1}>News</NavItem>
+              <NavItem eventKey={2}>Calendar</NavItem>
+              <NavItem eventKey={3}>Keys</NavItem>
+              <NavItem eventKey={4}>Rules</NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavDropdown eventKey={5} title="test" id="basic-nav-dropdown">
+                <MenuItem eventKey={5.1}>My profile</MenuItem>
+                <MenuItem eventKey={5.2}>Settings</MenuItem>
+              </NavDropdown>
+              <NavItem eventKey={6}>Logout</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={MainPage} />
+            <Route
+              exact
+              path="/studentunions"
+              component={() => <div>Student unions</div>}
+            />
+            <Route
+              path="/keyholders"
+              component={() => <div>Key holders</div>}
+            />
+            <Route path="/users" component={() => <div>Users</div>} />
+            <Route path="/rules" component={() => <div>Rules</div>} />
+            <Route path="/news" component={() => <div>News</div>} />
+            <Route path="/login" component={() => <div>Login</div>} />
+            <Route path="/logout" component={() => <div>Logout</div>} />
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { logger } from "./reducers/middleware";
+import testReducer from "./reducers/testReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Combine reducers
+const reducer = combineReducers({
+  test: testReducer
+});
+
+const middleware =
+  process.env.NODE_ENV !== "production"
+    ? [require("redux-immutable-state-invariant").default(), thunk, logger]
+    : [thunk];
+
+// Create store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
